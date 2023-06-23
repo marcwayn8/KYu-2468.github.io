723e7dcbda1b5b2607528be676400ce0251edc19
Wayne March <93327533+marcwayn8@users.noreply.github.com>
2023-06-23 12:16:48 -0400

    refactored 3d model

--- app/components/KevinAvatar.jsx
+++ app/components/KevinAvatar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Avatar } from "@readyplayerme/visage";
 import { useMediaQuery } from "usehooks-ts";
 import LoadingTriangle from "./Loading/LoadingTriangle";
-import { TypeAnimation } from "react-type-animation";
+
 
 const modelSrc = "https://models.readyplayer.me/6495962b8c331a0f7672b37d.glb"; // this can be a relative or absolute URL
 const animationSrc = "https://models.readyplayer.me/6495962b8c331a0f7672b37d.glb";
@@ -14,23 +14,24 @@ export default function KevinAvatar() {
   const isSmallScreen = useMediaQuery("(max-width: 768px)");
 
   return (
+   
     <Avatar
-    ambientLightColor="#fff5b6"
-    ambientLightIntensity={0.25}
-    cameraInitialDistance={2.5}
-    cameraTarget={1.65}
-    dirLightColor="#002aff"
-    dirLightIntensity={5}
-    emotion={{
-      browInnerUp: 0.3,
-      browOuterUpLeft: 0.37,
-      browOuterUpRight: 0.49,
-      eyeSquintLeft: 0.4,
-      eyeSquintRight: 0.2,
-      mouthShrugUpper: 0.27,
-      mouthSmileLeft: 0.37,
-      mouthSmileRight: 0.36
-    }}
+      ambientLightColor="#fff5b6"
+      ambientLightIntensity={0.25}
+      cameraInitialDistance={2.5}
+      cameraTarget={1.55}
+      dirLightColor="#002aff"
+      dirLightIntensity={5}
+      emotion={{
+        browInnerUp: 0.3,
+        browOuterUpLeft: 0.37,
+        browOuterUpRight: 0.49,
+        eyeSquintLeft: 0.4,
+        eyeSquintRight: 0.2,
+        mouthShrugUpper: 0.27,
+        mouthSmileLeft: 0.37,
+        mouthSmileRight: 0.36
+      }}
       environment="city"
       onLoaded={function noRefCheck(){}}
       onLoading={function noRefCheck(){}}
@@ -39,8 +40,10 @@ export default function KevinAvatar() {
       spotLightAngle={0.314}
       spotLightColor="#fff5b6"
       spotLightIntensity={1}
+    
       style={{
-        background: 'transparent',
+        background: 'transparent'
+   ,
         height: isSmallScreen ? "160px" : "516px"
       }}
       loader={<LoadingTriangle />}

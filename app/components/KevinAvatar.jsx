ad407d240f7d8df65a574d7204ed47fb30fb1e64
Wayne March <93327533+marcwayn8@users.noreply.github.com>
2023-06-23 10:09:40 -0400

    refactored 3d model

--- app/components/KevinAvatar.jsx
+++ app/components/KevinAvatar.jsx
@@ -14,43 +14,40 @@ export default function KevinAvatar() {
   const isSmallScreen = useMediaQuery("(max-width: 768px)");
 
   return (
- 
     <Avatar
-      ambientLightColor="#fff5b6"
-      ambientLightIntensity={0.25}
-      cameraInitialDistance={2.5}
-      cameraTarget={1.55}
-    
-      dirLightColor="#002aff"
-      dirLightIntensity={5}
-
-      emotion={{
-        browInnerUp: 0.3,
-        browOuterUpLeft: 0.37,
-        browOuterUpRight: 0.49,
-        eyeSquintLeft: 0.4,
-        eyeSquintRight: 0.2,
-        mouthShrugUpper: 0.27,
-        mouthSmileLeft: 0.37,
-        mouthSmileRight: 0.36
-      }}
+    ambientLightColor="#fff5b6"
+    ambientLightIntensity={0.25}
+    cameraInitialDistance={2.5}
+    cameraTarget={1.65}
+    dirLightColor="#002aff"
+    dirLightIntensity={5}
+    emotion={{
+      browInnerUp: 0.3,
+      browOuterUpLeft: 0.37,
+      browOuterUpRight: 0.49,
+      eyeSquintLeft: 0.4,
+      eyeSquintRight: 0.2,
+      mouthShrugUpper: 0.27,
+      mouthSmileLeft: 0.37,
+      mouthSmileRight: 0.36
+    }}
       environment="city"
       onLoaded={function noRefCheck(){}}
       onLoading={function noRefCheck(){}}
       modelSrc={modelSrc}
       animationSrc={animationSrc}
       poseSrc={poseSrc}
-    
       scale={1}
       spotLightAngle={0.314}
       spotLightColor="#fff5b6"
       spotLightIntensity={1}
-
       style={{
-        background: 'transparent'
+        background: 'transparent',
+   
+        height: isSmallScreen ? "160px" : "516px"
       }}
+      loader={<LoadingTriangle />}
     />
-
   );
 }
 

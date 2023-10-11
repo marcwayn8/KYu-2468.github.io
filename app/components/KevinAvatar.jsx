18113121409e768e207ee38c0d52ea9411e199ca
Wayne March <93327533+marcwayn8@users.noreply.github.com>
2023-10-11 12:34:09 -0400

    refactored 3d model

--- app/components/KevinAvatar.jsx
+++ app/components/KevinAvatar.jsx
@@ -12,51 +12,80 @@ export default function KevinAvatar() {
   const isSmallScreen = useMediaQuery("(max-width: 768px)");
 
   return (
+    // <Avatar
+    //   ambientLightColor="#fff5b6"
+    //   ambientLightIntensity={0.25}
+    //   animationSrc={animationSrc}
+    //   cameraInitialDistance={2.5}
+    //   cameraTarget={1.6}
+    //   cameraZoomTarget={[
+    //     {
+    //       x: 0,
+    //       y: 0.5,
+    //       z: 0.48,
+    //     },
+    //   ]}
+    //   dirLightColor="#002aff"
+    //   dirLightIntensity={5}
+    //   dirLightPosition={{
+    //     x: -3,
+    //     y: 5,
+    //     z: -5,
+    //     "_constructor-name_": "Vector3",
+    //   }}
+    //   environment="city"
+    //   headMovement
+    //   idleRotation
+    //   modelSrc={modelSrc}
+    //   onLoaded={function noRefCheck() {}}
+    //   onLoading={function noRefCheck() {}}
+    //   scale={1.1}
+    //   shadows
+    //   spotLightAngle={0.314}
+    //   spotLightColor="#fff5b6"
+    //   spotLightIntensity={1}
+    //   spotLightPosition={[
+    //     {
+    //       x: 12,
+    //       y: 10,
+    //       z: 7.5,
+    //     },
+    //   ]}
+    //   style={{
+    //     background: "transparent",
+    //     height: isSmallScreen ? "160px" : "516px",
+    //   }}
+    //   loader={<LoadingTriangle />}
+    // />
+
     <Avatar
-      ambientLightColor="#fff5b6"
-      ambientLightIntensity={0.25}
-      animationSrc={animationSrc}
-      cameraInitialDistance={2.5}
-      cameraTarget={1.6}
-      cameraZoomTarget={[
-        {
-          x: 0,
-          y: 0.5,
-          z: 0.48,
-        },
-      ]}
-      dirLightColor="#002aff"
-      dirLightIntensity={5}
-      dirLightPosition={{
-        x: -3,
-        y: 5,
-        z: -5,
-        "_constructor-name_": "Vector3",
-      }}
-      environment="city"
-      headMovement
-      idleRotation
-      modelSrc={modelSrc}
-      onLoaded={function noRefCheck() {}}
-      onLoading={function noRefCheck() {}}
-      scale={1.1}
-      shadows
-      spotLightAngle={0.314}
-      spotLightColor="#fff5b6"
-      spotLightIntensity={1}
-      spotLightPosition={[
-        {
-          x: 12,
-          y: 10,
-          z: 7.5,
-        },
-      ]}
-      style={{
-        background: "transparent",
-        height: isSmallScreen ? "160px" : "516px",
-      }}
-      loader={<LoadingTriangle />}
-    />
+    animationSrc={animationSrc}
+  background={{
+    color: 'rgb(9,20,26)'
+  }}
+  cameraInitialDistance={3.2}
+  cameraTarget={1.55}
+  modelSrc={modelSrc}
+ 
+  onLoadedEffect={{
+    loop: 12,
+    src : modelSrc
+  }}
+>
+  <Sparkles
+    color="white"
+    count={50}
+    opacity={0.9}
+    scale={5}
+    size={0.5}
+    speed={0.35}
+    style={{
+      background: "transparent",
+      height: isSmallScreen ? "160px" : "516px",
+    }}
+    loader={<LoadingTriangle />}
+  />
+</Avatar>
   );
 }
 

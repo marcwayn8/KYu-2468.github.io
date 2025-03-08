aa6ae636220b4fe6c2ee40476eba3d3727ab5de3
I41544 <i41544@verisk.com>
2025-03-08 11:31:36 -0500

    removed business email and name

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -52,11 +52,7 @@ const Contact = () => {
           buttonColor="bg-green-600"
           linkURL="tiktok.com/@juniormarch"
         /> */}
-         <div className="pb-12 mx-6 text-lg md:mx-16 md:text-xl">
-        Business Email: marcwayn84@gmail.com
-        Business Phone: (347) 670-4768
-        Business Address: 2810 N Church St #270756, Wilmington Delaware
-      </div>
+       
       </div>
     </>
   );
